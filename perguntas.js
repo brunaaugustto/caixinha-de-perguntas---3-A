378c41aa5fb5942122d79ee6962c07949839ac7e
brunaaugustto <165961633+brunaaugustto@users.noreply.github.com>
2025-10-01 09:07:34 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -8,3 +8,23 @@ criaCartao(
     'Qual a montanha mais alta do mundo?',
     'Monte Everest'
 )
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.'
+)
+criaCartao(
+    'Lógica',
+    'O que é mais pesado: 1 quilo de algodão ou 1 quilo de ferro?',
+    'Ambos têm o mesmo peso: 1 quilo. Mas, como precisamos de uma porção de algodão maior para compor 1 quilo, temos a sensação de que o mesmo peso de algodão é mais leve do que o chumbo.'
+)
+criaCartao(
+    'Curiosidades',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison.A lâmpada foi inventada por Thomas Edison (1847-1931) em 1879. No dia 21 de outubro, o inventor conseguiu manter uma lâmpada acessa durante 48 horas.'
+)
+criaCartao(
+    'Física',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'A Terra demora aproximadamente 24 horas, mais precisamente 23 horas, 56 minutos e 4 segundos para dar uma volta completa em torno do seu próprio eixo. Esse movimento recebe o nome de rotação.'
+)
